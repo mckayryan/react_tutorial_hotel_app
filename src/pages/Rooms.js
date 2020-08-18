ee5d892e6fcb23bd53ed60a696954e85aa6e9800
Ryan <z5060961@zmail.unsw.edu.au>
2020-08-18 13:28:18 +1000

    initial commit

---
+++ src/pages/Rooms.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Hero from "../components/Hero"
+import Banner from "../components/Banner"
+
+import {Link} from "react-router-dom"
+
+import { FaAlignRight } from 'react-icons/fa';
+
+const Rooms = () => {
+  return (
+    <Hero hero="roomsHero">
+      <Banner title="Our luxurious rooms">
+        <Link to='/' className="btn-primary">
+          Return home
+        </Link>
+      </Banner>
+    </Hero>
+  );
+}
+
+export default Rooms;
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './App.css';
+
+const SingleRoom = () => {
+  return (
+    <div>
+      Hello from Single room page
+    </div>
+  );
+}
+
+export default SingleRoom;
